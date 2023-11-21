ffece6f97d210c3b0ada5980684e6da95273a5ef
maniksheikh <mdmanik081498@gmail.com>
2023-11-22 01:09:33 +0600

    added the slider page in project

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// Swiper js 
+var swiper = new Swiper(".bg-slider-thumbs", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
